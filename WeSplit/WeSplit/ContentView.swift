da64718a5d06f09a467a64e8e0ae5e74dad6c267
Oytun TUR <turoytun0@gmail.com>
2025-01-29 21:27:55 +0300

    Added button and how to change a body variable

--- WeSplit/WeSplit/ContentView.swift
+++ WeSplit/WeSplit/ContentView.swift
@@ -8,6 +8,9 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var tapCount = 0
+    
+    
     var body: some View {
         NavigationStack{
             Form {
@@ -15,6 +18,9 @@ struct ContentView: View {
                     Text("Hello, World!")
                     Text("Hello, World!")
                     Text("Hello, World!")
+                    Button("Tap Count: \(tapCount)") {
+                        tapCount += 1
+                    }
                 }
                 Text("Hello, World!")
                 Text("Hello, World!")
