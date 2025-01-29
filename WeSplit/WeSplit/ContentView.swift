59035a8513f54864a32cdd76c0469a4317ab2400
Oytun TUR <turoytun0@gmail.com>
2025-01-29 21:17:08 +0300

    created wesplit project and added form view

---
+++ WeSplit/WeSplit/ContentView.swift
@@ -0,0 +1,30 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by Oytun Tur on 29.01.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Form {
+            Section{
+                Text("Hello, World!")
+                Text("Hello, World!")
+                Text("Hello, World!")
+            }
+            Text("Hello, World!")
+            Text("Hello, World!")
+            Text("Hello, World!")
+            Text("Hello, World!")
+            Text("Hello, World!")
+            Text("Hello, World!")
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Oytun Tur on 29.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
