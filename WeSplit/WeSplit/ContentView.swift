0a0539fe0ccee1849c9ea5ea597b9dfc2ec26fa9
Oytun TUR <turoytun0@gmail.com>
2025-01-29 21:51:21 +0300

    Reverted all changes for real project

--- WeSplit/WeSplit/ContentView.swift
+++ WeSplit/WeSplit/ContentView.swift
@@ -8,42 +8,10 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var tapCount = 0
-    
-    @State private var name = ""
-    
-    let students: [String] = ["Ali", "Veli", "Can", "Omer"]
-    
-    @State private var selectedStudent: String = "Ali"
-    
-    
     var body: some View {
-        NavigationStack{
-            Form {
-                Section{
-                    Button("Tap Count: \(tapCount)") {
-                        tapCount += 1
-                    }
-                }
-                Section{
-                    TextField("Enter Your Name",text: $name)
-                    Text("Your Name is: \(name)")
-                }
-                Section(){
-                    Picker("Select Your Student",selection: $selectedStudent){
-                        ForEach(students,id: \.self){
-                            Text($0)
-                        }
-                    }
-                }
-                }
-            }
-            .navigationTitle("WeSplit")
-            
-            
+        Text("Hello, World!")
         }
     }
-
 
 #Preview {
     ContentView()
