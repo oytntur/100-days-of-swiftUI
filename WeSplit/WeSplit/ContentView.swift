bd7b052308a17722223df50c019c01dac48e5359
Oytun TUR <turoytun0@gmail.com>
2025-01-30 16:14:07 +0300

    added header text to section

--- WeSplit/WeSplit/ContentView.swift
+++ WeSplit/WeSplit/ContentView.swift
@@ -32,7 +32,7 @@ struct ContentView: View {
       
                 }
                 
-                Section{
+                Section("How much do you want to tip?"){
                     Picker("Tip Percentage",selection: $tipPercentage){
                         ForEach(tipPercentages, id:\.self){
                             Text($0, format:.percent)
